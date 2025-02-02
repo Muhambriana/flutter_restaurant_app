3f35bfe057362949112f2b87f5d2e0dee17ebee4
muham <muhambriana.31@gmail.com>
2025-02-02 13:17:25 +0700

    refactor: remove unused code

--- lib/ui/restaurant_list_screen.dart
+++ lib/ui/restaurant_list_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant_app/data/model/restaurant.dart';
-import 'package:flutter_restaurant_app/provider/list/search_restaurant_list_provider.dart';
 import 'package:flutter_restaurant_app/static/restaurant_list_result_state.dart';
 import 'package:flutter_restaurant_app/ui/restaurant_detail_screen.dart';
 import 'package:flutter_restaurant_app/ui/widget/error_message.dart';
 import 'package:flutter_restaurant_app/ui/widget/loading_view.dart';
-import 'package:flutter_restaurant_app/ui/widget/search_bar.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_restaurant_app/provider/list/restaurant_list_provider.dart';
 
@@ -29,37 +27,20 @@ class _RestaurantListScreenState extends State<RestaurantListScreen> {
     });
   }
 
-  void onQueryChange(String keyword) {
-    setState(() {
-      Future.microtask(() {
-        context.read<SearchRestaurantsProvider>().searchRestaurantList(keyword);
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: RestaurantSearchBar(onQueryChanged: onQueryChange,),
+        title: Text(RestaurantListScreen.title),
       ),
-      body: Consumer2<RestaurantListProvider, SearchRestaurantsProvider>(
-          builder: (context, listProvider, searchProvider, child) {
-            if(searchProvider.restaurantName.isNotEmpty) {
-              return switch (searchProvider.resultState) {
-                RestaurantListLoadingState() => LoadingView(),
-                RestaurantListErrorState(error: var message) => ErrorMessage(message: message),
-                RestaurantListLoadedState(data: var restaurantList) => _listView(restaurantList),
-                _ => const SizedBox(),
-              };
-            } else {
-              return switch (listProvider.resultState) {
-                RestaurantListLoadingState() => LoadingView(),
-                RestaurantListErrorState(error: var message) => ErrorMessage(message: message),
-                RestaurantListLoadedState(data: var restaurantList) => _listView(restaurantList),
-                _ => const SizedBox(),
-              };
-            }
+      body: Consumer<RestaurantListProvider>(
+          builder: (context, value, child) {
+            return switch (value.resultState) {
+              RestaurantListLoadingState() => LoadingView(),
+              RestaurantListErrorState(error: var message) => ErrorMessage(message: message),
+              RestaurantListLoadedState(data: var restaurantList) => _listView(restaurantList),
+              _ => const SizedBox(),
+            };
           }
       ),
     );
