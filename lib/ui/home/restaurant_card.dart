926f5be208a27856c8b18c5556522647b2e069a0
Muham <muhambriana.31@gmail.com>
2025-02-09 21:40:32 +0700

    feat: update layout of list item

--- lib/ui/home/restaurant_card.dart
+++ lib/ui/home/restaurant_card.dart
@@ -17,7 +17,50 @@ class RestaurantCard extends StatelessWidget {
       onTap: onTap,
       child: Card(
         color: Colors.green,
-        child: Text(restaurant.name, style: TextStyle(fontSize: 50),),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Text(
+                    restaurant.name, style: TextStyle(fontSize: 20),
+                  )
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              flex: 3,
+                              child: Text(
+                                  'Name',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15
+                                  ),
+                              )
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Text('Rating')
+                          ),
+                        ],
+                      ),
+                      Text('Place'),
+                      Text('Desc'),
+                    ],
+                  )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/home/restaurant_list.dart
+++ lib/ui/home/restaurant_list.dart
@@ -11,27 +11,24 @@ class RestaurantList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.black,
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: restaurantList.length,  // Use the correct variable here
-        itemBuilder: (context, index) {
-          final restaurant = restaurantList[index];  // Use the correct variable here
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: restaurantList.length,  // Use the correct variable here
+      itemBuilder: (context, index) {
+        final restaurant = restaurantList[index];  // Use the correct variable here
 
-          return RestaurantCard(
-            onTap: () {
-              // Navigate to detail
-              Navigator.pushNamed(
-                context,
-                RestaurantDetailScreen.routeName,
-                arguments: restaurant.id, // Pass restaurant ID
-              );
-            },
-            restaurant: restaurant,
-          );
-        },
-      ),
+        return RestaurantCard(
+          onTap: () {
+            // Navigate to detail
+            Navigator.pushNamed(
+              context,
+              RestaurantDetailScreen.routeName,
+              arguments: restaurant.id, // Pass restaurant ID
+            );
+          },
+          restaurant: restaurant,
+        );
+      },
     );
   }
 }
