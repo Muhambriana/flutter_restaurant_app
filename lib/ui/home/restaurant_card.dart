13cfc64a2f4d81cf8dd3b098c5ac6c55cc627955
Muham <muhambriana.31@gmail.com>
2025-02-09 21:49:03 +0700

    feat: add image to list item

--- lib/ui/home/restaurant_card.dart
+++ lib/ui/home/restaurant_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant_app/data/model/restaurant.dart';
 
@@ -24,10 +25,16 @@ class RestaurantCard extends StatelessWidget {
             children: [
               Expanded(
                   flex: 1,
-                  child: Text(
-                    restaurant.name, style: TextStyle(fontSize: 20),
-                  )
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: CachedNetworkImage(
+                      imageUrl: 'https://pikaso.cdnpk.net/private/production/1235478040/render.jpeg?token=exp=1763078400~hmac=0362c7bc4eeec5940e2e9a9030a4ef379f0ead9af96087755160b4ea4641c621',
+                      placeholder: (context, url) => CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                  ),
               ),
+              SizedBox(width: 5),
               Expanded(
                   flex: 2,
                   child: Column(
