1b97c0323943fbda258711303d7dceddd340e28c
Muham <muhambriana.31@gmail.com>
2025-02-13 21:42:58 +0700

    feat: update layout of search bar

--- lib/ui/widget/general/search_bar.dart
+++ lib/ui/widget/general/search_bar.dart
@@ -10,21 +10,43 @@ class RestaurantSearchBar extends StatefulWidget {
 }
 
 class _RestaurantSearchBarState extends State<RestaurantSearchBar> {
-  String keyword = '';
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: TextField(
-        decoration: InputDecoration(
-          labelText: 'Find Restaurant Name',
-          border: OutlineInputBorder(),
-          prefixIcon: Icon(Icons.search_rounded),
+    return Card(
+      elevation: 10,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            const Icon(Icons.search, color: Colors.grey), // Search icon
+            const SizedBox(width: 8),
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  hintText: "Search restaurants...",
+                  border: InputBorder.none, // Removes underline
+                ),
+                onChanged: widget.onQueryChanged, // Callback on text change
+              ),
+            ),
+            if (_controller.text.isNotEmpty)
+              IconButton(
+                icon: const Icon(Icons.clear, color: Colors.grey),
+                onPressed: () {
+                  _controller.clear();
+                  widget.onQueryChanged('');
+                  setState(() {}); // Refresh UI
+                },
+              ),
+          ],
         ),
-        onChanged: widget.onQueryChanged,
       ),
     );
   }
 }
+
 
