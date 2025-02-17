d87056d020bd1f5ce7e5f3dbf87eb81779711bf5
Muham <muhambriana.31@gmail.com>
2025-02-17 20:14:30 +0700

    feat: add button back in search_restaurant_screen.dart

--- lib/static/routes.dart
+++ lib/static/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_restaurant_app/ui/search/search_retaurant_screen.dart';
+import 'package:flutter_restaurant_app/ui/search/search_restaurant_screen.dart';
 import '../ui/detail/restaurant_detail_screen.dart';
 import '../ui/home/home_screen.dart';
 import '../ui/main_screen.dart';
